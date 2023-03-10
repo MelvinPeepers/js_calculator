0488fcc1ddfd2eb15d1437f202ebf850b9f1ccfb
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-03-10 11:38:09 -0500

    adding in JS

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const buttons = document.querySelectorAll("button")
 const screenDisplay = document.querySelectorAll(".display")
+
+// console.log(buttons)
+
+let calculation = []
+
+function calculate(button) {
+    // console.log(button)
+    const value = button.textContent
+    calculation.push(value)
+    screenDisplay.textContent = calculation
+    console.log(calculation)
+}
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
