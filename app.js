433af018f6e7e9d1e4797ed505023a0313561207
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-03-10 14:04:00 -0500

    finished JS

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const buttons = document.querySelectorAll("button")
-const screenDisplay = document.querySelectorAll(".display")
+const screenDisplay = document.querySelector(".screen")
+const buttons = document.querySelectorAll("button");
 
+
+// console.log(screenDisplay)
 // console.log(buttons)
 
 let calculation = []
+let accumlativeCalculation 
 
 function calculate(button) {
     // console.log(button)
     const value = button.textContent
-    calculation.push(value)
-    screenDisplay.textContent = calculation
-    console.log(calculation)
+
+    if (value === "Clear"){
+        calculation = []
+        screenDisplay.textContent = "."
+    } else if (value === "=") {
+        screenDisplay.textContent = eval(accumlativeCalculation)
+    } else {
+        calculation.push(value)
+        accumlativeCalculation = calculation.join("")
+        screenDisplay.textContent = accumlativeCalculation
+    }
+    
+
+
+    // console.log(calculation)
 }
 
-buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
+buttons.forEach(button => button.addEventListener("click", () => calculate(button)))
